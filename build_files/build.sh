51754afe84d31896821a8aea76f8790b12b85f1c
Julian Giamblanco <giamblanco.julian@gmail.com>
2026-01-04 18:36:37 -0800

    install NVM from `bulid.sh`

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
+
+# install Node Version Manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 # Use a COPR Example:
 #
