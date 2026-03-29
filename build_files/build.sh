94b33f7981fd9fd3e24b3c2e3b60ee7b4ce70bcd
Julian Giamblanco <giamblanco.julian@gmail.com>
2026-03-28 18:22:16 -0700

    add some more flatpaks

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,11 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux rust cargo helix
 
-flatpak install --system -y flathub com.bambulab.BambuStudio
+flatpak install --system -y flathub \
+    com.bambulab.BambuStudio \
+    chat.delta.desktop \
+    org.freecad.FreeCAD \
+    org.wezfurlong.wezterm
 
 # install Node Version Manager
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
